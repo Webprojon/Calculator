d0118f734e945246c40796b61271b8981eadd2d6
Webprojon <tokhirjonkhasanov@gmail.com>
2024-10-27 19:56:20 +0100

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      //{
+      //  test: /\.css$/,
+      //  use: ['style-loader', 'css-loader'],
+      //},
+    ],
+  },
+};
